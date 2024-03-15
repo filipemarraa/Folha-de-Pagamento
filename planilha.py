54a35dd5f12a990bd8388428e3501fd5abc81dd8
Pedro <pedronasserog@gmail.com>
2024-03-15 15:16:47 -0300

    codigo para transformar a planilha filtrada

--- planilha.py
+++ planilha.py
@@ -1,18 +1,27 @@
 import pandas as pd
 
 # Caminho para o seu arquivo CSV
-caminho_do_arquivo = '/Users/filipemarra/Downloads/atividade-implementacao-dados.csv'
+caminho_do_arquivo = 'atividade-implementacao-dados.csv'
 
-# Lendo o arquivo CSV
+# Lendo o arquivo CSV de dados bruto
 df = pd.read_csv(caminho_do_arquivo)
 
-# Filtrando as colunas desejadas
+# Filtrando e renomeando as colunas desejadas do dado bruto
 colunas_desejadas = ['Funcionários', 'Banco', 'Cargo', 'Salário', 'Dias trabalhados em abril', 'Salário família']
 df_filtrado = df[colunas_desejadas]
-df_filtrado.rename(columns={'Funcionários': 'colaborador', 'Dias trabalhados em abril': 'dias_trab', 'Banco': 'banco', 'Salário família': 'salario_fami'}, inplace=True)
+df_filtrado.rename(columns={'Funcionários': 'colaborador', 'Dias trabalhados em abril': 'dias_trab', 'Banco': 'banco', 'Salário família': 'salario_fami', 'Cargo': 'cargo'}, inplace=True)
 
+# Lendo CSV separado de cargos x salarios
+df_cargo_sal = pd.read_csv('planilha_cargo_salario.csv')
 
-# Salvando o dataframe filtrado em um novo arquivo CSV
-df_filtrado.to_csv('planilha_limpa.csv', index=False)
+# Mesclando a planilha filtrada com a de cargos x salarios
+df_mesclado = pd.merge(df_filtrado, df_cargo_sal, on='cargo', how='left')
 
-print("A planilha foi filtrada e salva como 'planilha_limpa.csv'.")
+# Filtrando e renomeando colunas da tabela definitiva
+filtrando_definitivo = ['colaborador', 'salário', 'dias_trab', 'salario_fami', 'banco']
+df_final = df_mesclado[filtrando_definitivo]
+df_final.rename(columns={'salário': 'cargo_salario'})
+
+# Salvando o arquivo final
+df_final.to_csv('planilha_final.csv', index=False)
+print("A planilha foi filtrada e salva como 'planilha_final.csv'.")
