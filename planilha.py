bedb7f7cbf05ee6bb997ecfd3ce1dd8f006abca4
Pedro <pedronasserog@gmail.com>
2024-03-19 16:40:51 -0300

    otimizando e finalizando código

--- planilha.py
+++ planilha.py
@@ -6,22 +6,24 @@ caminho_do_arquivo = 'atividade-implementacao-dados.csv'
 # Lendo o arquivo CSV de dados bruto
 df = pd.read_csv(caminho_do_arquivo)
 
-# Filtrando e renomeando as colunas desejadas do dado bruto
-colunas_desejadas = ['Funcionários', 'Banco', 'Cargo', 'Salário', 'Dias trabalhados em abril', 'Salário família']
-df_filtrado = df[colunas_desejadas]
-df_filtrado.rename(columns={'Funcionários': 'colaborador', 'Dias trabalhados em abril': 'dias_trab', 'Banco': 'banco', 'Salário família': 'salario_fami', 'Cargo': 'cargo'}, inplace=True)
+# Primeira filtragem do CSV original
+colunas_desejadas = ['Funcionários', 'Cargo', 'Salário', 'Dias trabalhados em abril', 'Salário família', 'Banco']
+df_csv1 = df[colunas_desejadas]
+df_csv1.rename(columns={'Funcionários': 'colaborador', 'Cargo': 'cargo', 'Dias trabalhados em abril': 'dias_trab', 'Salário família': 'salario_fami', 'Banco': 'banco'}, inplace=True)
 
-# Lendo CSV separado de cargos x salarios
-df_cargo_sal = pd.read_csv('planilha_cargo_salario.csv')
+# Renomeando profissões 
+df_csv1.loc[:, 'cargo'] = df_csv1['cargo'].str.replace('Programadora júnior', 'Programador júnior')
+df_csv1.loc[:, 'cargo'] = df_csv1['cargo'].str.replace('Programadora plena', 'Programador pleno')
+df_csv1.loc[:, 'cargo'] = df_csv1['cargo'].str.replace('Programadora sênior', 'Programador sênior')
 
-# Mesclando a planilha filtrada com a de cargos x salarios
-df_mesclado = pd.merge(df_filtrado, df_cargo_sal, on='cargo', how='left')
+# Criando outra tabela Cargo/Salario para mesclar
+colunas_desejadas_2 = ['Cargos', 'Salário']
+df_csv2 = df[colunas_desejadas_2]
+df_csv2.rename(columns={'Cargos': 'cargo', 'Salário': 'salario'}, inplace=True)
 
-# Filtrando e renomeando colunas da tabela definitiva
-filtrando_definitivo = ['colaborador', 'salario', 'dias_trab', 'salario_fami', 'banco']
-df_final = df_mesclado[filtrando_definitivo]
+# Mesclagem
+df_mesclado = pd.merge(df_csv1, df_csv2, on='cargo', how='left')
+colunas_desajadas_final = ['colaborador', 'salario', 'dias_trab', 'salario_fami', 'banco']
+df_final = df_mesclado[colunas_desajadas_final]
 df_final.rename(columns={'salário': 'cargo_salario'}, inplace=True)
-
-# Salvando o arquivo final
 df_final.to_csv('planilha_final.csv', index=False)
-print("A planilha foi filtrada e salva como 'planilha_final.csv'.")
